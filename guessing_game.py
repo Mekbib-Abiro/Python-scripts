d30d4a1d4be6df02522e3336b2daa90e214bc412
Mekbib Abiro <133805365+Mekbib-Abiro@users.noreply.github.com>
2024-01-23 20:44:53 +0300

    Add files via upload

---
+++ guessing_game.py
@@ -0,0 +1,44 @@
+import random
+
+def four_digit_guess():
+    list_1 = []
+    for digit in range(4):
+        digit = random.randint(0, 9)
+        list_1.append(str(digit))
+    target = ''.join(list_1)
+    
+    print('You have 10 chances to guess a randomly generated number using the info provided.')
+    guess = input('Guess a number: ')
+    while len(guess) != 4:
+        guess = input('Type a 4-digit number.\nGuess a number: ')
+    count = 1
+    
+    while guess != target:
+        correct_place = 0
+        incorrect_place = 0
+        tempo = target
+        for i in range(4):
+            if guess[i] == target[i]:
+                correct_place += 1
+                tempo = tempo.replace(guess[i], '')
+        for i in range(4):
+            if guess[i] in tempo:
+                incorrect_place += 1
+                tempo = tempo.replace(guess[i], '')
+        print(
+            f'Number guessed right and in the correct place: {correct_place}')
+        print(
+            f'Number guessed right but not in correct place: {incorrect_place}')
+        guess = input('Guess again: ')
+        while len(guess) != 4:
+            guess = input('Type a 4-digit number.\nGuess again: ')
+        count += 1
+        
+        if count == 10:
+            print(f'Sorry! you did not find it. the number was {target}.')
+            break   
+        
+        print(f'Congratulations you found the number! you took {count} guesses')
+
+if __name__ == '__main__':
+    four_digit_guess()
